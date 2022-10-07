526d83086da7163e936ac2175e21605275a58ab6
hazem237 <201082@ppu.edu.ps>
2022-10-07 19:58:54 +0300

    moce folder 4 from master

---
+++ 4/Solution.ts
@@ -0,0 +1,51 @@
+interface User {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+interface Admin {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+];
+// " is" is a good way to say that it's an admin person or User person 
+// if the function return true , then will arrow to the interface 
+
+export function isAdmin(person: Person): person is Admin {
+    return person.type === 'admin';
+}
+
+export function isUser(person: Person): person is User {
+    return person.type === 'user';
+}
+
+export function logPerson(person: Person) {
+    let additionalInformation: string = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
+
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
